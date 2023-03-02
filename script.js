a46ee178acc0f1a53940a31edc5134f637215f63
Yireobong William <willflash014@gmail.com>
2023-03-02 22:54:12 +0000

    modified html,css and js files

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var hamburger = document.querySelector(".hamburger");
+var navList = document.querySelector(".nav-list");
+var navLink = document.querySelectorAll(".nav-link");
+console.log(navLink);
 hamburger.addEventListener("click", myFunction);
 
+
 function myFunction() {
-    if (hamburger.classList.contains("active") == false ) {
-        hamburger.classList.add("active");
-    } else {
+    if (navList.classList.contains("active") == false && hamburger.classList.contains("active") == false) {
+        navList.classList.toggle("active");
+        hamburger.classList.toggle("active");
+    }
+    else {
+        navList.classList.remove("active");
         hamburger.classList.remove("active");
     }
-};
+}
